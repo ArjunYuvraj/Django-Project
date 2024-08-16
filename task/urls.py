ad670ab73ddcc07154f3ded69cc337664c653c45
ArjunYuvraj <Arjunyuvaraj17@gmail.com>
2024-08-16 16:22:52 +0530

    added statics files

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from task import views
+from .views import *
 
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
+    path('base/',views.base,name='base'),
 ]
