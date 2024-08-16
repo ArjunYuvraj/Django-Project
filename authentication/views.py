63f03a8effe3c4404bace2cfac3f14de396adac6
ArjunYuvraj <Arjunyuvaraj17@gmail.com>
2024-08-16 20:35:00 +0530

    feat: added new feature to create user and save it in the database

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User   
 
 # Create your views here.
 def loginpage(request):
@@ -8,4 +9,11 @@ def loginpage(request):
     return render(request, 'login.html')
 
 def signup_page(request):
+    if request.method == "POST":
+        
+        new_user = User( username= request.POST['username'],
+                         first_name= request.POST['firstname'],
+                         last_name=request.POST['lastname'],)
+        new_user.set_password(request.POST['password'])
+        new_user.save()
     return render(request, 'signup.html')
