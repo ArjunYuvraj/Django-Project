74d79b701fd3359404c00e4047314226defc485a
ArjunYuvraj <Arjunyuvaraj17@gmail.com>
2024-08-16 20:46:15 +0530

    feat: implemented login/signup feature and credentials authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from .models import User   
+from django.shortcuts import render,redirect
+from .models import User 
+from django.contrib.auth import authenticate, login,logout  
 
 # Create your views here.
 def loginpage(request):
     if request.method == "POST":
         print(request.POST)
+        user=authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request,user)
+            return redirect ('task/home/')
 
     return render(request, 'login.html')
 
