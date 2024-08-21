b5d39e86c03401bf6f719b99c4fb63e0f71c1370
ArjunYuvraj <Arjunyuvaraj17@gmail.com>
2024-08-21 18:18:33 +0530

    feature: craeted a feature to add status of the task

--- task/forms.py
+++ task/forms.py
@@ -4,4 +4,4 @@ from .models import Task
 class Taskform(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'category']  # Specify the fields to include in the form
+        fields = ['title', 'description', 'due_date', 'category','status']  # Specify the fields to include in the form
--- task/models.py
+++ task/models.py
@@ -1,16 +1,25 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+        ('Needs Review', 'Needs Review'),
+    ]
+    
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    due_date = models.DateField()    
+    description = models.TextField(blank=True, default=" no descrition available")
+    due_date = models.DateField(default=date.today)    
     category = models.CharField(max_length=50, choices=[
         ('work', 'Work'),
         ('personal', 'Personal'),
         ('urgent', 'Urgent'),
         ('other', 'Other'),
     ])
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES) 
     
     def __str__(self):
         return self.title
